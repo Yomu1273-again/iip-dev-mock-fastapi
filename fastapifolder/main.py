3b88a16e1d7137c8345ae9aec6b44c35caba8205
Yomu1273 <87906136+Yomu1273@users.noreply.github.com>
2024-07-31 15:34:58 +0530

    mongo connection

--- fastapifolder/main.py
+++ fastapifolder/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://yogeshmuneese:yogeshmuneese@cluster1.bqp7ddk.mongodb.net/")
+db = client["IIP_TEST"]
+collection = db["USERS"]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items")
+async def read_items():
+    items = list(collection.find())
+    print(items)
+    item_object = []
+    for item in items:
+        item.pop('_id', None)
+        item_object.append(item)
+    return item_object
